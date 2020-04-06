0070c81e8c241203e8ed054b939bda521d889dec
Jessica Oliveira <jessie_oliveira@icloud.com>
2020-04-06 15:04:52 -0400

    Added a callback class to listen for changes in the custom edit text field

---
+++ BBLocations/app/src/main/java/com/example/bblocations/utils/listeners/GenericListener.java
@@ -0,0 +1,7 @@
+package com.example.bblocations.utils.listeners;
+
+import android.view.KeyEvent;
+
+public interface GenericListener {
+    public void searchListener(CharSequence text, int i, KeyEvent keyEvent);
+}
