ed01dd00bbf205b70722311ffa05b8f200d95164
Jessica Oliveira <jessie_oliveira@icloud.com>
2020-04-07 16:10:03 -0400

    Added unit tests for searching on listview. Added instrumental tests for checking elements visibility and ids. Fixed filter function in Utils

---
+++ BBLocations/app/src/main/java/com/example/about/AboutPresenterImpl.java
@@ -0,0 +1,59 @@
+package com.example.about;
+
+import android.content.Context;
+import android.os.Handler;
+import java.lang.ref.WeakReference;
+import androidx.annotation.NonNull;
+
+/**
+ * Created by Backbase R&D B.V on 28/06/2018.
+ */
+
+public class AboutPresenterImpl implements About.Presenter {
+
+    private final WeakReference<About.View> aboutView;
+    private final AboutModelImpl aboutModel;
+
+    public AboutPresenterImpl(About.View view, @NonNull Context context){
+        this.aboutView = new WeakReference<>(view);
+        this.aboutModel = new AboutModelImpl(this, context);
+    }
+
+    @Override
+    public void getAboutInfo() {
+        About.View aboutViewImpl = aboutView.get();
+
+        aboutViewImpl.showProgress();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                aboutModel.getAboutInfo();
+            }
+        }, 1000);
+    }
+
+    @Override
+    public void onSuccess(AboutInfo aboutInfo) {
+        About.View aboutViewImpl = aboutView.get();
+
+        if(aboutViewImpl != null){
+            aboutViewImpl.hideProgress();
+            aboutViewImpl.setCompanyName(aboutInfo.getCompanyName());
+            aboutViewImpl.setCompanyAddress(aboutInfo.getCompanyAddress());
+            aboutViewImpl.setCompanyPostalCode(aboutInfo.getCompanyPostal());
+            aboutViewImpl.setCompanyCity(aboutInfo.getCompanyCity());
+            aboutViewImpl.setAboutInfo(aboutInfo.getAboutInfo());
+        }
+
+    }
+
+    @Override
+    public void onFail() {
+        About.View aboutViewImpl = aboutView.get();
+        if (aboutViewImpl != null){
+            aboutViewImpl.hideProgress();
+            aboutViewImpl.showError();
+        }
+    }
+}
